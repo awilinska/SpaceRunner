e91877b77cf3e766768946e3551a4edc4c805ba2
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-10-12 12:43:20 +0200

    Adding comments

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -1,5 +1,4 @@
-/* Script for the bullets shooting by space ship. 
-The bullet is destroying after collision with object tagged as "Obstacle" */
+// Script for the bullets shooting by space ship. 
 
 using System.Collections;
 using System.Collections.Generic;
@@ -10,12 +9,12 @@ public class Bullet : MonoBehaviour
     public float speed;
 
     private void Update() {
-        transform.Translate(transform.right * transform.localScale.x * speed * Time.deltaTime);
+        transform.Translate(transform.right * transform.localScale.x * speed * Time.deltaTime); // Moving
     }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Obstacle")) {
-            Destroy(gameObject);
+            Destroy(gameObject); // Bullet will be destroyed after hitting object tagged as "Obstacle"
         }
     }
 }
