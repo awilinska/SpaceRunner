21fc1453771047fde77ef53e61e1698e397ec2fc
Alicja Wilińska <72753653+awilinska@users.noreply.github.com>
2022-10-12 13:04:07 +0200

    Adding comments

--- Scripts/Station.cs
+++ Scripts/Station.cs
@@ -1,3 +1,5 @@
+// Script for the space station.
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,14 +11,14 @@ public class Station : MonoBehaviour
     public GameObject stationSound;
 
     private void Update() {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(Vector2.left * speed * Time.deltaTime); // Movement
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            Instantiate(stationSound, transform.position, Quaternion.identity);
-            other.GetComponent<Player>().armor = 100;
-            other.GetComponent<Shooting>().bulletCount = 3;
+        if (other.CompareTag("Player")) { // If collide with the object tagged as "Player:
+            Instantiate(stationSound, transform.position, Quaternion.identity); // make station sound
+            other.GetComponent<Player>().armor = 100; // reset space ship armor to 100
+            other.GetComponent<Shooting>().bulletCount = 3; // reset number of bullets to 3
             Debug.Log(other.GetComponent<Player>().armor);
         }
     }
